1678e50ff6d792ef38163d67f672d79380d741fe
Lakshna S <slakshna2000@gmail.com>
2021-12-04 17:51:20 +0530

    first commit

---
+++ src/app/Service/covid.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CovidService {
+
+  constructor(private http:HttpClient) { }
+
+  getCountries():Observable<any>{
+    const url = "https://api.covid19api.com/countries"
+    return this.http.get<any>(url);
+  }
+
+  getCoronaRealData(country:string):Observable<any>{
+    const url = "https://api.covid19api.com/total/country/"+ country
+    return this.http.get<any>(url)
+  }
+
+  getTotal():Observable<any>{
+  const url = "https://api.covid19api.com/world/total"
+  return this.http.get<any>(url)
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CovidService } from './Service/covid.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,56 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CovidAPI';
+  countries:any
+  country:any
+  Confirmed:Number=0;
+  Recovered:Number=0;
+  Deaths:Number=0;
+  Date:String='';
+  Active:Number=0;
+  Country:String='';
+  TotalConfirmed:Number=0;
+  TotalDeaths:Number=0;
+  TotalRecovered:Number=0;
+
+  constructor(private corona:CovidService){}
+
+  ngOnInit(){
+    this.corona.getCountries().subscribe((data:any)=>{
+    console.log(data)
+    this.countries = data
+    })
+
+    this.getworldtotal()
+  }
+  getCoronaData(){
+    this.corona.getCoronaRealData(this.country).subscribe((data:any)=>{
+      console.log(data)
+      var index = data.length-1;
+      for(var i=0;i<=index;i++)
+      {
+        this.Confirmed=this.Confirmed+data[i].Confirmed;
+        this.Recovered=this.Recovered+data[i].Recovered;
+        this.Deaths=this.Deaths+data[i].Deaths;
+      }
+      this.Date = data[index].Date
+      this.Active = data[index].Active
+      this.Country = data[index].Country
+
+    })
+  }
+
+  getCountry(country:any){
+    this.country = country
+  }
+
+  getworldtotal(){
+    this.corona.getTotal().subscribe((data:any)=>{
+      console.log(data)
+
+      this.TotalConfirmed = data.TotalConfirmed
+      this.TotalDeaths = data.TotalDeaths
+      this.TotalRecovered = data.TotalRecovered
+    })
+  }
 }
